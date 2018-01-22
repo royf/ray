1281ac3da1dc48ce177177b86915e6079a6548df
Eric Liang <ekhliang@gmail.com>
2018-01-21 21:26:51 -0800

    Sun Jan 21 21:26:51 PST 2018

--- python/ray/rllib/bc/policy.py
+++ python/ray/rllib/bc/policy.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import numpy as np
+
 import ray
 import tensorflow as tf
 from ray.rllib.a3c.policy import Policy
